c196d03cf9141e8fac5a91a4a1c5795bda919233
Vex0on <Iplayagame101@gmail.com>
2024-01-15 01:14:49 +0100

    Fully working solution input

--- Cryptography_3D_Game/Assets/Scripts/Player/PlayerMove.cs
+++ Cryptography_3D_Game/Assets/Scripts/Player/PlayerMove.cs
@@ -1,99 +1,106 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] float walkSpeed = 10f;
-    [SerializeField] float runSpeed = 15f;
-    [SerializeField] Animator animator;
+    [SerializeField] private float walkSpeed = 10f;
+    [SerializeField] private float runSpeed = 15f;
+    [SerializeField] private Animator animator;
+    [SerializeField] private TMP_InputField messageInputField;
 
-    Vector2 moveInput;
-    Rigidbody rb;
-    bool isModalOpen = false;
+    private Vector2 moveInput;
+    private Rigidbody rb;
+    private bool isModalOpen = false;
+    private bool isMessageInputFocused = false;
 
     private Coroutine inputChecker;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        if (messageInputField != null)
+        {
+            messageInputField.onSelect.AddListener(OnMessageInputSelect);
+            messageInputField.onDeselect.AddListener(OnMessageInputDeselect);
+        }
     }
 
     void Update()
     {
-        if (!isModalOpen)
+        if (!isModalOpen || isMessageInputFocused)
         {
             HandleMovement();
             UpdateAnimator();
         }
     }
 
+    private void OnMessageInputSelect(string value)
+    {
+        isMessageInputFocused = true;
+    }
+
+    private void OnMessageInputDeselect(string value)
+    {
+        isMessageInputFocused = false;
+    }
+
     private void HandleMovement()
     {
-        Vector3 playerVelocity = new Vector3(moveInput.x, 0, moveInput.y);
-        playerVelocity.Normalize();
+        Vector3 playerVelocity = new Vector3(moveInput.x, 0, moveInput.y).normalized;
         Vector3 moveDirection = transform.TransformDirection(playerVelocity);
 
-        float speed = walkSpeed;
-
-        if (Keyboard.current.leftShiftKey.isPressed)
-        {
-            speed = runSpeed;
-        }
-
+        float speed = Keyboard.current.leftShiftKey.isPressed ? runSpeed : walkSpeed;
         rb.velocity = moveDirection * speed;
     }
 
     private void UpdateAnimator()
     {
-        float moveMagnitude = new Vector2(moveInput.x, moveInput.y).magnitude;
+        float moveMagnitude = moveInput.magnitude;
         animator.SetBool("isWalking", moveMagnitude > 0);
         animator.SetBool("isRunning", moveMagnitude > 0 && Keyboard.current.leftShiftKey.isPressed);
     }
 
     private void OnEquipment(InputValue value)
     {
-        if (Keyboard.current.iKey.isPressed)
+        if (Keyboard.current.iKey.wasPressedThisFrame)
         {
-            SetPlayerMovementEnabled(!isModalOpen);
-
-            if (isModalOpen)
+            if (!isMessageInputFocused)
             {
-                if (inputChecker != null)
-                    StopCoroutine(inputChecker);
-                inputChecker = StartCoroutine(CustomInputCheck());
-            }
+                SetPlayerMovementEnabled(!isModalOpen);
 
-            StartCoroutine(ToggleEquipmentModalAfterInputCheck());
+                if (isModalOpen && inputChecker != null)
+                {
+                    StopCoroutine(inputChecker);
+                    inputChecker = StartCoroutine(CustomInputCheck());
+                }
+
+                StartCoroutine(ToggleEquipmentModalAfterInputCheck());
+            }
         }
     }
 
     private IEnumerator ToggleEquipmentModalAfterInputCheck()
     {
         yield return new WaitForEndOfFrame();
-
         EquipmentModalManager.Instance.ToggleEquipmentModal();
     }
 
     private IEnumerator CustomInputCheck()
     {
-        yield return new WaitForSeconds(0.2f);
-
-        while (EventSystem.current.currentSelectedGameObject == gameObject)
+        while (isModalOpen)
         {
             while (Keyboard.current.leftCtrlKey.isPressed && Keyboard.current.backspaceKey.isPressed)
             {
                 Debug.Log("Wprowadzono 'i' w inpucie podczas otwartego modala");
-
                 yield return new WaitForSeconds(0.2f);
             }
 
             yield return null;
         }
     }
-
 
     private void SetPlayerMovementEnabled(bool isEnabled)
     {
